e6cb88dcf1baf83c33c09c9887c89d9457929b81
fardeen awais <fardeenjutt99@gmail.com>
2023-10-24 14:40:37 -0700

    Making it static progress bar

--- components/Str01.tsx
+++ components/Str01.tsx
@@ -5,17 +5,16 @@ import { motion, useInView} from 'framer-motion'
 import { slideAnimation } from '@/lib/motion'
 
 const Storyline01 = () => {
-  const ref = useRef(null)
-  const isInView = useInView(ref);
+
   return (
-    <motion.div ref={ref} variants={slideAnimation("left")} initial="initial" exit="exit" whileInView="animate" className='flex flex-col gap-8 max-w-7xl justify-center items-center min-h-screen px-5 '>
+    <motion.div variants={slideAnimation("left")} initial="initial" exit="exit" whileInView="animate" className='flex flex-col gap-8 max-w-7xl justify-center items-center min-h-screen px-5 '>
       <div className='flex flex-col justify-center items-center max-w-xl p-4'>
         <h2>Welcome to Alfarnex</h2>
         <p className='text-center'>{`Hi There 👋 We are helping business to without top class developers and Designers. We can relief a lot of your Business Stress 😩`}</p>
       </div>
       <div className='flex flex-col md:flex-row items-center justify-center gap-10 px-4 py-8 bg-opacity-50 shadow-lg backdrop-blur-[4px] backdrop-filter transition-opacity duration-200 ease-in-out rounded-2xl'>
         <div>
-        <WebVitals value={90} isInView={isInView} />
+        <WebVitals value={90} />
         </div>
         <p className='max-w-md'>{`We make interactive web applications that result to increase of more than 80% of Success 🎉`}</p>
       </div>
--- components/subcomponents/webvitals.tsx
+++ components/subcomponents/webvitals.tsx
@@ -1,57 +1,34 @@
+"use client";
+
 import { useInView } from "framer-motion";
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { CircularProgress } from "@nextui-org/react";
 
-interface WebVitalsProps {
-  value: number;
-  isInView: boolean;
-}
-
-const WebVitals = ({ value,isInView }: any) => {
-  
+const WebVitals = ({ value}:any)=> {
   const [values, setValue] = useState(value);
-  console.log({value, isInView})
+  const ref = useRef(null)
+  const isInView = useInView(ref)
 
-  // if (isInView === true) {
-  //   const interval = setInterval(() => {
-  //     setValue((v: number) => (v >= values ? values : v + 10));
-  //   }, 1500);
-  //   return () => clearInterval(interval);
-  // }
-
-  const interval = setInterval(() => {
-    if (isInView === true) {
-      setValue((v: number) => (v >= value ? value : v + 10));
-      return () => {
-        clearInterval(interval);
-      };
-    }
-    else if (isInView === false) {
-      setValue((v: number) => (v >= value ? 0 : v + 10));
-      return () => {
-        clearInterval(interval);
-      };
-    }
-  }, 1500);
-
-  // React.useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setValue((v:any) => (v >= value ? 0 : v + 10));
-  //   }, 500);
-
-  //   return () => clearInterval(interval);
-  // }, [isInView]);
+  if (isInView === true) { // I want to 
+    const interval = setInterval(() => {
+      setValue((v: any) => (v >= values ? values : v + 10));
+    }, 9000);
+    clearInterval(interval);
+  }
 
   return (
     <div className="relative h-full w-full">
       <CircularProgress
+      ref={ref}
         aria-label="Loading..."
-        classNames={{
-          svg: "w-36 h-36 drop-shadow-md",
+        classNames={
+          {
+          svg: "w-36 h-36 drop-shadow-md ",
           track: "stroke-black/10",
           value: "text-3xl font-semibold text-green-900 dark:text-green-300",
           indicator: "shadow-xl",
-        }}
+        }
+      }
         size="lg"
         value={values}
         color="success"
@@ -59,6 +36,6 @@ const WebVitals = ({ value,isInView }: any) => {
       />
     </div>
   );
-};
+}
 
 export default WebVitals;
